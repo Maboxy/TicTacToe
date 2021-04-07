f4583a5d617a511d2b470575ef517fa828ad7de8
Maboxy <paul@hackenberg.info>
2021-04-07 19:32:27 +0200

    Initial commit

---
+++ TicTacToe/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TicTacToe
+//
+//  Created by Paul Hackenberg on 07.04.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
