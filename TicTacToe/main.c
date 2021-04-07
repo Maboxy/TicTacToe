60816db373ea37502be8de48f1a93e13dbf1d5ea
Maboxy <paul@hackenberg.info>
2021-04-07 20:12:15 +0200

    Erste Spielfeld Ausgabe

--- TicTacToe/main.c
+++ TicTacToe/main.c
@@ -16,9 +16,18 @@ struct Spielfeld{
     int iFeld[3][3];
 };
 
+void ausgeben(struct Spielfeld *spielfeldAusgebenptr){//Funktion Will einen Pointer auf ein Spielfeld struct
+    for(int i = 0; i < 3; i++){         //Durch For-Schliefe kann jedes einzelne Feld ausgegeben werden
+        for(int j = 0; j < 3; j++){
+            printf("%d ", spielfeldAusgebenptr->iFeld[i][j]);
+        }
+    }
+    printf("\n");
+}
+
 
 int main(int argc, const char * argv[]) {
-    printf("Welcome to TictacToe in C");
+    printf("Welcome to TictacToe in C \n");
     struct Spielfeld spielfeldObjekt;
     struct Spielfeld *spielfeldptr = &spielfeldObjekt;  //bekommen die Speicheraddresse von spielfeldObjekt
     
@@ -37,6 +46,8 @@ int main(int argc, const char * argv[]) {
     spieler1.cZeichen = 'O';
     spieler1.iIndexer = 2;
     struct Spieler *spieler2ptr = &spieler2;
+    
+    ausgeben(spielfeldptr); //Aufruf der Funktion "ausgeben" dadurch wird das Spielfeld in der Konsole ausgegeben
     
     return 0;
 }
